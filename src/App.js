8374f9fd0c9beb5f7ef1425c1558a2f2d2ffbfcc
marciemaclean <macleanphoenix@gmail.com>
2021-07-26 15:46:16 -0400

    elements aligned and added image

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div class = 'app'>
-      <h1>Welcome to Potluck Planner!</h1>
-      <span class= 'buttons'>
-        <button  class= 'login'>Login</button>
-        <button class= 'signup'>Sign Up</button>
-      </span> 
+    <div className = 'App'>
+      <div className='topContent'>
+       <h1>Welcome to the Potluck Planner!</h1>
+          <img  className= 'grillImg' src= 'https://i.kym-cdn.com/photos/images/original/001/556/116/4fb.png' alt = 'Get grilling!' width='300vh'/>
+      </div>
+      <div className= 'buttons'>
+        <button  className= 'login'>Login</button>
+        <button className= 'signup'>Sign Up</button>
+      </div> 
     </div>
   );
 }
-
+const grillImgStyle = {
+ 
+}
 export default App;
