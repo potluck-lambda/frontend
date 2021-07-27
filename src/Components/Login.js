c4a0d60c09bab026cc573091c64eac4a7f8e1c95
JacobGriffithElliott <j.o.gelliott@gmail.com>
2021-07-26 19:19:39 -0700

    Completed simple login form, still needs alot of work. Added a Component folder, and Login.js file.

---
+++ src/Components/Login.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+      error: '',
+    };
+
+    this.PassChange = this.PassChange.bind(this);
+    this.UserChange = this.UserChange.bind(this);
+    this.Submit = this.Submit.bind(this);
+    this.RemoveError = this.RemoveError.bind(this);
+  }
+
+  UserChange(event) {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+
+  PassChange(event) {
+    this.setState({
+      password: event.target.value,
+    });
+  }
+
+  RemoveError() {
+    this.setState({ error: '' });
+  }
+
+  Submit(event) {
+    event.preventDefault();
+
+    if (!this.state.username) {
+      return this.setState({ error: 'Username is required' });
+    }
+
+    if (!this.state.password) {
+      return this.setState({ error: 'Password is required' });
+    }
+
+    return this.setState({ error: '' });
+  }
+
+ 
+  render() {
+   
+
+    return (
+      <div className="Login">
+        <form onSubmit={this.Submit}>
+          {
+            this.state.error &&
+            <h2 data-test="error" onClick={this.RemoveError}>
+              <button onClick={this.RemoveError}>X</button>
+              {this.state.error}
+            </h2>
+          }
+          <label>User Name</label>
+          <input type="text" data-test="username" value={this.state.username} onChange={this.UserChange} />
+
+          <label>Password</label>
+          <input type="password" data-test="password" value={this.state.password} onChange={this.PassChange} />
+
+          <input type="submit" value="Log In" data-test="submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
