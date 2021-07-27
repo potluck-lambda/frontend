45780a3b02695af508a206778e088db30109aba6
marciemaclean <macleanphoenix@gmail.com>
2021-07-27 12:30:40 -0400

    created homepage component

--- src/App.js
+++ src/App.js
@@ -11,34 +11,4 @@ import Login from './Components/Login'
 
 import { CreateEvent } from './Components/CreateEvent'
 
-
-function App() {
-  return (
-<<<<<<< HEAD
-    <div className = 'App'>
-      <div className='topContent'>
-       <h1>Welcome to the Potluck Planner!</h1>
-          <img  className= 'grillImg' src= 'https://i.kym-cdn.com/photos/images/original/001/556/116/4fb.png' alt = 'Get grilling!' width='300vh'/>
-      </div>
-      <div className= 'buttons'>
-        <button  className= 'login'>Login</button>
-        <button className= 'signup'>Sign Up</button>
-      </div> 
-    </div>
-=======
-    <div>
-        <Switch>
-          {/* <Route exact path='/' component={HomepagePlaceholder}/>
-          <Route exact path='/sign-up' component={SignUpPlaceholder}/> */}
-          <Route exact path='/login' component={Login}/>
-          {/* <Route exact path='/protected/eventlist' component={EventListPlaceholder}/> */}
-          <Route path='/protected/createevent' component={CreateEvent}/>
-          {/* <Route exact path='/protected/editevent' component={EditEventPlaceholder} />
-          <Route exact path='/protected/eventdetails' component={EventDetailsPlaceholder}/> */}
-        </Switch>
-      </div>
->>>>>>> main
-  );
-}
-
 export default App;
