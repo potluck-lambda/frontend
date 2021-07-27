d36188c5777ac7f6c473594f58c38eb13099523d
Leu Family <romanianfobjr@gmail.com>
2021-07-27 10:49:31 -0500

    yup installed

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ function App() {
           {/* <Route exact path='/' component={HomepagePlaceholder}/>
           <Route exact path='/sign-up' component={SignUpPlaceholder}/> */}
           <Route exact path='/login' component={Login}/>
-          {/* <Route exact path='/protected/eventlist' component={EventListPlaceholder}/>
-          <Route exact path='/protected/createevent' component={CreateEventPlaceholder}/>
-          <Route exact path='/protected/editevent' component={EditEventPlaceholder} />
+          {/* <Route exact path='/protected/eventlist' component={EventListPlaceholder}/> */}
+          <Route path='/protected/createevent' component={CreateEvent}/>
+          {/* <Route exact path='/protected/editevent' component={EditEventPlaceholder} />
           <Route exact path='/protected/eventdetails' component={EventDetailsPlaceholder}/> */}
         </Switch>
       </div>
