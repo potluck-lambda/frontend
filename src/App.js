46a376fe970367fc7e6bebeecf327d076db5b023
Leu Family <romanianfobjr@gmail.com>
2021-07-27 10:35:38 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import './App.css';
+import {Switch, Route} from "react-router-dom";
+import React from 'react';
+// import HomepagePlaceholder from ''
+import Login from './Components/Login'
+// import SignUpPlaceholder from ''
+// import EventListPlaceholder from ''
+// import CreateEventPlaceholder from ''
+// import EditEventPlaceholder from ''
+// import EventDetailsPlaceholder from ''
 
 import { CreateEvent } from './Components/CreateEvent'
 
