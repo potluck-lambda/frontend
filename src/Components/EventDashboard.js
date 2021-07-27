50a7723065eff4fedb31c12f500c8eeadbba93f7
Leu Family <romanianfobjr@gmail.com>
2021-07-27 17:07:20 -0500

    button added to EventDashboard that takes a user to /protected/createevent AKA CreateEvent

--- src/Components/EventDashboard.js
+++ src/Components/EventDashboard.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 import EventCard from './EventCard'
 
@@ -21,6 +22,10 @@ function EventDashboard(props) {
     return (
         <div>
             <h2>Potlucks</h2>
+            <Link to='/protected/createevent'>
+                <button>Create new potluck</button>
+            </Link>
+            
             {
                 events.map(ev => {
                     return (
